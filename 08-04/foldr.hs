9e872d37978531cb74cae9cb7776d06118676715
jeniffer borges <jenifferrborges@gmail.com>
2022-04-08 18:58:49 -0300

    08-04

--- 08-04/foldr.hs
+++ 08-04/foldr.hs
@@ -4,3 +4,6 @@ tamanho (x:xs) = 1 + tamanho xs
 
 tamanhov2 :: [a] -> Int
 tamanhov2 = foldr (\x acc -> 1 + acc) 0
+
+maximo :: Ord a => [a] -> a
+maximo = foldr1 (\x max -> if x > max then x else max)
