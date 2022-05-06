3709e1f8c758b2c06d0f8fd7c1df89f994ca2934
Jeniffer M Borges <74516572+JenifferMB@users.noreply.github.com>
2022-05-06 18:48:08 -0300

    06-05

--- 06-05/data2.hs
+++ 06-05/data2.hs
@@ -1,1 +1,8 @@
-data Shape = Circle Float | Rectangle Float Float deriving (Show)
+data Shape = Circle Float | Rectangle Float Float deriving (Show) -- funcoes construtoras
+
+square :: Float -> Shape
+square n = Rectangle n n 
+
+area :: Shape -> Float
+area (Circle r) = pi*r^2
+area (Rectangle b h) = b*h
